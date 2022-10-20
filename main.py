fb9b3819c308f72affe9ac6bd923694d5dde4640
Ziyad Mir <ziyadmir@gmail.com>
2022-10-20 07:27:42 +0000

    add /new command to test out slash comands

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 import discord
 import os
 from pyairtable import Table
+from discord import app_commands
+import random
+
 
 # setup discord connection
 intents = discord.Intents(messages=True, guilds=True)
 intents.message_content = True
 discord_bot_client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(discord_bot_client)
+
 
 # setup airtable connection
 airtable_api_key = os.environ["AIRTABLE_API_KEY"]
-table = Table(airtable_api_key, 'app8xDpApplv8WrVJ', 'interview')
-table.all()
+interview_table = Table(airtable_api_key, 'app8xDpApplv8WrVJ', 'interview')
+question_table = Table(airtable_api_key, 'app8xDpApplv8WrVJ', 'questions')
 
+# constants
+GUILD_ID = 1032341469551415318 # discord server id
 
 @discord_bot_client.event
 async def on_ready():
+    await tree.sync(guild=discord.Object(id=GUILD_ID))
     print(f'We have logged in as {discord_bot_client.user}')
 
 
@@ -26,8 +34,14 @@ async def on_message(message):
     if message.author == discord_bot_client.user:
         return
 
-    table.create({"name": message.author.name, "message": message.content})
+    interview_table.create({"name": message.author.name, "message": message.content})
     await message.channel.send(message.content[::-1])
+
+
+@tree.command(name = "new", description = "Get a new question", guild=discord.Object(id=GUILD_ID))
+async def new_command(interaction):
+    random_question = random.choice(question_table.all())
+    await interaction.response.send_message("Here's your question: {}".format(random_question['fields']['link']))
 
 
 discord_bot_client.run(os.getenv("TOKEN"))
