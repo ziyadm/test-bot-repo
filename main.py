d93bb754ad80ad91dda5c9a3df177bf4a9734535
prointerview <116355587+prointerviewschool@users.noreply.github.com>
2022-10-23 02:37:28 +0000

    main.py small cleanup

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-import discord
+import events
+import slash_commands
+
 import os
-from slash_commands import new_command, submit_command, delete_command
-from events import on_member_join_event, on_presence_update_event
+
+import discord
 
 # ================================
 # === setup discord connection ===
 # ================================
-intents = discord.Intents(messages=True, guilds=True, message_content=True, members=True, presences=True)
+intents = discord.Intents(messages=True,
+                          guilds=True,
+                          message_content=True,
+                          members=True,
+                          presences=True)
 discord_client = discord.Client(intents=intents)
 tree = discord.app_commands.CommandTree(discord_client)
 
@@ -24,21 +30,21 @@ discord_guild = discord.Object(id=os.environ['discord_guild_id'])
               guild=discord_guild)
 async def register_new_command(interaction):
   await interaction.response.defer()
-  return await new_command(interaction)
+  return await slash_commands.new_command(interaction)
 
 @tree.command(name="submit",
               description="Submit",
               guild=discord_guild)
 async def register_submit_command(interaction):
   await interaction.response.defer()
-  return await submit_command(interaction)
+  return await slash_commands.submit_command(interaction)
 
 @tree.command(name="delete",
               description="dete",
               guild=discord_guild)
 async def register_delete_command(interaction):
   await interaction.response.defer()
-  return await delete_command(interaction)
+  return await slash_commands.delete_command(interaction)
 
 # ================================
 # === register events ============
@@ -51,10 +57,10 @@ async def on_ready():
   
 @discord_client.event
 async def on_member_join(member):
-    return await on_member_join_event(member)
+    return await events.on_member_join_event(member)
 
 @discord_client.event
 async def on_presence_update(before, after):
-    return await on_presence_update_event(before, after)
+    return await events.on_presence_update_event(before, after)
 
 discord_client.run(os.environ['discord_secret_token'])
