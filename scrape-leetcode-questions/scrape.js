89567590ad74cf1877fc0a2548bcbab0f97106e3
prointerviewschool <prointerviewschool@gmail.com>
2022-11-02 08:10:52 -0400

    _

--- scrape-leetcode-questions/scrape.js
+++ scrape-leetcode-questions/scrape.js
@@ -1,13 +1,40 @@
+function make_url(path_parts) {
+    return _.flatten([path_parts]).join('/');
+}
+
 class Question_id {
     constructor({url}) {
         this.value = url.split('/')[2]
     }
-
-    get url() {
-        return 'https://www.leetcode.com/problems/' + this.value
-    }
 };
 
+class Questions_page {
+    constructor(number) {
+        this.number = number
+    }
+
+    get __url() {
+        return make_url(['?page=' + String(this.number)]);
+    }
+
+    async with_close({f}) {
+        let page = window.open(this.__url);
+        await new Promise(function(resolve) {
+            page.onload = function () {
+                console.log('url after loading' + page.location.href);
+                console.log($(page.document).find('[role="rowgroup"]').find('[role="row"]').map(function(index, row) {
+                    return new Question_id({
+                        url: $(row).find('a').first().attr('href')
+                    });
+                }));
+                resolve();
+            };
+        });
+        let result = f(page);
+        page.close();
+        return result;
+    }
+}
 
 class Question {
     constructor({id, name, tags, description}) {
@@ -18,7 +45,37 @@ class Question {
     }
 };
 
+async function run_after({f, seconds, args}) {
+    await new Promise(function (resolve) {
+        _.delay(resolve, seconds * 1000)
+    });
+    return f(args);
+}
 
-const question_ids = $('[role="rowgroup"]').find('[role="row"]').map(function(index, row) {
-    return new Question_id({
-        url: $(row).find('a').first().attr('href')})});
+function question_ids_on_page({questions_page}) {
+    return questions_page.with_close({
+        f: function(page) {
+            return $(page.document).find('[role="rowgroup"]').find('[role="row"]').map(function(index, row) {
+                return new Question_id({
+                    url: $(row).find('a').first().attr('href')
+                });
+            });
+        }})
+}
+
+const seconds_between_requests_to_avoid_getting_rate_limited = 2;
+
+const number_of_questions_pages = parseInt($('.grid').find('[role="navigation"]').find('[aria-label="next"]').prev().text());
+
+const question_ids = await _.range(number_of_questions_pages, number_of_questions_pages + 1).reduce(async function(question_ids_promise, page_number) {
+    const question_ids = await question_ids_promise;
+    const questions_page = new Questions_page(page_number);
+    const question_ids_on_this_page = await run_after({
+        f: question_ids_on_page,
+        seconds: seconds_between_requests_to_avoid_getting_rate_limited,
+        args: {questions_page: questions_page}});
+    console.log(question_ids_on_this_page);
+    return question_ids.concat(question_ids_on_this_page);
+}, []);
+
+console.log(question_ids)
--- scrape-leetcode-questions/setup.js
+++ scrape-leetcode-questions/setup.js
@@ -1,4 +1,9 @@
-const jquery_script = document.createElement("script");
-jquery_script.type = "text/javascript";
-jquery_script.src = "https://code.jquery.com/jquery-3.6.1.slim.min.js";
-document.head.appendChild(jquery_script);
+function import_from_url(url) {
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = url;
+    document.head.appendChild(script);
+}
+
+import_from_url("https://code.jquery.com/jquery-3.6.1.slim.min.js");
+import_from_url("https://cdn.jsdelivr.net/npm/underscore@1.13.6/underscore-umd-min.js");
